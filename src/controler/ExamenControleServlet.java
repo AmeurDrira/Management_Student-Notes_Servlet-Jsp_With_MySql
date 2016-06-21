6b434fc3c48b0c313986920558e3a4590bf541c0
ameur92 <ameur.drira@gmail.com>
2016-06-21 05:47:29 +0100

    « AjoutSessionRattrapage »

--- src/controler/ExamenControleServlet.java
+++ src/controler/ExamenControleServlet.java
@@ -1,11 +1,31 @@
 package controler;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.implementes.EtudiantImpl;
+import dao.implementes.GroupeImpl;
+import dao.implementes.MatiereImpl;
+import dao.implementes.MatiereensignierImpl;
+import dao.implementes.NoteImpl;
+import dao.implementes.SessionImpl;
+import model.Ensignant;
+import model.Etudiant;
+import model.Groupe;
+import model.Matiere;
+import model.Matiereensignier;
+import model.Note;
+import model.Session;
 
 /**
  * Servlet implementation class ExamenControleServlet
@@ -26,15 +46,97 @@ public class ExamenControleServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		MatiereensignierImpl matiereensignierImpl = new MatiereensignierImpl();
+		GroupeImpl groupeImpl = new GroupeImpl();
+		MatiereImpl matiereImpl = new MatiereImpl();
+		List<Matiereensignier> list;
+		List<Note> listeNote;
+		List<Matiere> listeMatiere;
+		List<Groupe> listeGroupe;
+		Ensignant e;
+
+		HttpSession session;
+		session = request.getSession();
+		listeGroupe = groupeImpl.getAllGroupe();
+		listeMatiere = matiereImpl.getAllMatiere();
+
+		e = (Ensignant) session.getAttribute("ensignant");
+		list = matiereensignierImpl.getAllMatiereensignierParEnsignant(e);
+		request.setAttribute("ensignant", e);
+		request.setAttribute("list", list);
+		request.setAttribute("listeGroupe", listeGroupe);
+		request.setAttribute("listeMatiere", listeMatiere);
+
+		String idgroup = request.getParameter("idgroupe");
+		String idmatiere = request.getParameter("idmatiere");
+
+		if (!(null == idgroup) && !(null == idmatiere)) {
+			Matiere matiere = matiereImpl.findByIdMatiere(Integer.parseInt(idmatiere));
+			Groupe groupe = groupeImpl.findByIdGroupe(Integer.parseInt(idgroup));
+			NoteImpl noteImpl = new NoteImpl();
+			listeNote = noteImpl.getAllNoteByGroupMatiere(groupe, matiere);
+			request.setAttribute("listeNote", listeNote);
+			request.setAttribute("matiere", matiere);
+			request.setAttribute("groupe", groupe);
+		}
+
+		RequestDispatcher rd = getServletContext().getRequestDispatcher("/EspaceEnsignantControleView.jsp");
+		rd.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		MatiereensignierImpl matiereensignierImpl = new MatiereensignierImpl();
+		GroupeImpl groupeImpl = new GroupeImpl();
+		MatiereImpl matiereImpl = new MatiereImpl();
+		EtudiantImpl etudiantImpl = new EtudiantImpl();
+		NoteImpl noteImpl = new NoteImpl();
+		SessionImpl sessionImpl = new SessionImpl();
+
+		List<Note> listeNote;
+		Ensignant e;
+		HttpSession session = request.getSession();
+		Enumeration param = request.getParameterNames();
+		List headerValuesList = Collections.list(param);
+
+		int idmatiere = Integer.parseInt(request.getParameter("idmatiere"));
+		int idgroupe = Integer.parseInt(request.getParameter("idgroupe"));
+
+		e = (Ensignant) session.getAttribute("ensignant");
+
+		Matiere matiere = matiereImpl.findByIdMatiere(idmatiere);
+
+		Groupe groupe = groupeImpl.findByIdGroupe(idgroupe);
+		listeNote = noteImpl.getAllNoteByGroupMatiere(groupe, matiere);
+
+		Session sessionObj = sessionImpl.findSessionControle();
+		Matiereensignier matiereensignier;
+
+		matiereensignier = matiereensignierImpl.findMatiereensignierParMatGroupEns(matiere, groupe, e);
+
+		for (Note note : listeNote) {
+			Etudiant etudiantObj = etudiantImpl.findByIdEtudiant(note.getEtudiant().getId());
+			float noteprincipale = 0;
+
+			for (Object headerValueObj : headerValuesList) {
+				String fieldName = headerValueObj.toString();
+				if (String.valueOf(note.getNumcompostage()).equals(fieldName))
+					noteprincipale = Float.parseFloat(request.getParameter(fieldName));
+
+			}
+			if (noteprincipale != 0) {
+				System.out.println("-----------------");
+				System.out.println(noteprincipale);
+				System.out.println(note.getEtudiant().getNom());
+				System.out.println("-----------------");
+				note = new Note(note.getId(), noteprincipale, note.getNotepresentielle(), note.getNotetd(),
+						note.getNotetp(), note.getNumcompostage(), etudiantObj, sessionObj, matiereensignier);
+				noteImpl.updateNote(note);
+			}
+
+		}
 		doGet(request, response);
 	}
 
